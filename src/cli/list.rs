0e85ed81f7336c5fc8959588b5ff594efd5c6ec6
Bohdan Strutynskyi <strutinsky5386@gmail.com>
2020-09-21 14:13:17 +0300

    Make list command read the currently installed candidates

--- src/cli/list.rs
+++ src/cli/list.rs
@@ -3,6 +3,7 @@ use crate::sdkman_api::candidates;
 use crate::sdkman_api::candidates::fetch_candidates;
 use crate::sdkman_api::versions::fetch_versions;
 use crate::engine::operating_system::get_current_os_and_arch;
+use crate::engine::filesystem::get_installed_candidate_versions;
 
 pub fn build_cli_list() -> Command {
     Command::new("list")
@@ -14,8 +15,8 @@ pub fn exec_list(c: &Context) {
     if(c.args.len() > 0) {
         let candidate_name = c.args[0].clone();
         let os_and_arch = get_current_os_and_arch();
-        let current_version = c.args[1].clone();
-        let installed_versions = &c.args[2..]; // TODO
+        let current_version = c.args[1].clone(); // TODO
+        let installed_versions = get_installed_candidate_versions(candidate_name.clone());
         println!("Listing versions for [{}] {}", os_and_arch, candidate_name);
         let versions =
             fetch_versions(candidate_name, os_and_arch, current_version, Vec::from(installed_versions)).unwrap();
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,4 +1,4 @@
 pub mod install_sdkit;
 pub mod use_sdkit;
-mod filesystem;
+pub(crate) mod filesystem;
 pub mod operating_system;
